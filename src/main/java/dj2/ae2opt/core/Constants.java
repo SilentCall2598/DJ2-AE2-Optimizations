17665df5350972d4646f0f06684359f1c78926df
SilentCall2598 <csntemp.alarcon@gmail.com>
2026-09-24 13:39:38 -0400

    Release 1.0.0

--- src/main/java/dj2/ae2opt/core/Constants.java
+++ src/main/java/dj2/ae2opt/core/Constants.java
@@ -4,7 +4,7 @@ public final class Constants {
 
     public static final String MOD_ID = "dj2ae2opt";
     public static final String MOD_NAME = "DJ2 AE2 Optimizations";
-    public static final String VERSION = "0.5.0-dev";
+    public static final String VERSION = "1.0.0";
 
     private Constants() {
     }
